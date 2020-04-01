695da86e0800bdeaba16ba0bdc9dfbd137252708
hgurnel <hgurnel@gmail.com>
2020-04-01 17:20:44 +0200

    Test merge sort

--- GenericSort/main.cpp
+++ GenericSort/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int main()
 {
-    int myArr[8] = {7, 5, 4, 3, 2, 1, 0, 6};
-    Array<int> a(myArr, 8);
-    
+    int myArr[9] = {7, 5, 4, 3, 2, 1, 0, 6, -1};
+    Array<int> a(myArr, 9);
+    a.mergeSort(0,8);
+    a.print();
     return 0;
 }
